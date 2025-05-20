04dc318c18274441ee78f5d517b7b041a012a1eb
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-20 10:29:49 +0700

    add while choise !=5

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -78,7 +78,13 @@ int main ()
 
     while (choise != 5)
     {
-        
+        cout << "1. push\n";
+        cout << "2. pop\n";
+        cout << "3. peek\n";
+        cout << "4. exit\n";
+        cout << "enter your choice : ";
+        cin >> choise;
+
     }
 }
 
