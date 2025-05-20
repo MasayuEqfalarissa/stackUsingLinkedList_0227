123056fe1533c30287b78e4ffb190b3a970da567
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-20 10:35:49 +0700

    membuat case 2

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -92,6 +92,17 @@ int main ()
             cin>> value;
             stact.push(value);
             break;
+
+            case 2: 
+            if (!stact.isEmpty())
+            {
+                stact.pop();
+            }
+            else
+            {
+                cout << "stact is Empty. cannot pop." << endl;
+            }
+            break;
         }
     }
 }
