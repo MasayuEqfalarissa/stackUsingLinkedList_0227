4182e18649281cfbb8ec570fe9635a4d50d589cf
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-20 10:14:23 +0700

    add void peek

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -44,4 +44,12 @@ public :
         cout << "popped value: " << top->data << endl;
         top = top->next;
     }
+
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "list is Empty" << endl;
+        }
+    }
 };
